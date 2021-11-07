b3b3b9a4b20473d59caf3bf0cd8217c3251ea649
tkmynrg <dlsenok@mail.ru>
2021-11-08 01:36:27 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/js/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.bundle.js',
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: "only",
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/html/template.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './src/fonts',
+                    to: './fonts'
+                },
+                {
+                    from: './src/favicon',
+                    to: './favicon'
+                },
+                {
+                    from: './src/img',
+                    to: './img'
+                },
+                {
+                    from: './src/documents',
+                    to: './documents'
+                }
+            ]
+        }),
+        // применять изменения только при горячей перезагрузке
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+          }),
+    ],
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            // изображения
+            {
+
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // шрифты и SVG
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            // CSS, PostCSS, Sass
+            {
+                test: /\.(s?css|css)$/,
+                use: [MiniCssExtractPlugin.loader, 
+                {loader: 'css-loader', options: {sourceMap: true}},
+                {loader: 'postcss-loader', options: {sourceMap: true}},
+                {loader: 'sass-loader', options: {sourceMap: true}},
+                ]
+            }
+        ],
+    }
+}
