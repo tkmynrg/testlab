e02ad51355c57ed86de9a46e70c5f6aba7c80861
tkmynrg <dlsenok@mail.ru>
2021-11-15 23:54:31 +0400

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: "./css/style.css"
+            filename: "style.css"
         }),
         new CopyWebpackPlugin({
             patterns: [
